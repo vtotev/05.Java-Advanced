267e116099f7a9d3415eede67662fee41fd825ad
Valeri Totev <valerivt@yahoo.com>
2021-03-13 11:33:13 +0200

    add

---
+++ 07.Generics/CustomListIterator/Main.java
@@ -0,0 +1,46 @@
+package Generics.CustomListIterator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        CustomList<String> list = new CustomList<>();
+        String input = scan.nextLine();
+        while (!input.equalsIgnoreCase("end")) {
+            String[] cmd = input.split("\\s+");
+            switch (cmd[0]) {
+                case "Add":
+                    list.add(cmd[1]);
+                    break;
+                case "Remove":
+                    list.remove(Integer.parseInt(cmd[1]));
+                    break;
+                case "Contains":
+                    System.out.println(list.contains(cmd[1]));
+                    break;
+                case "Swap":
+                    list.swap(Integer.parseInt(cmd[1]), Integer.parseInt(cmd[2]));
+                    break;
+                case "Greater":
+                    System.out.println(list.getGreatherThan(cmd[1]));
+                    break;
+                case "Max":
+                    System.out.println(list.getMax());
+                    break;
+                case "Min":
+                    System.out.println(list.getMin());
+                    break;
+                case "Sort":
+                    list.sort();
+                    break;
+                case "Print":
+                    for (String str: list) {
+                        System.out.println(str);
+                    }
+                    break;
+            }
+            input = scan.nextLine();
+        }
+    }
+}
