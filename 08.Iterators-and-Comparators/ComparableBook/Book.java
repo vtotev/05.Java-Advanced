72f9bb412c14a29f9786e29f52b88500e2069da4
Valeri Totev <valerivt@yahoo.com>
2021-04-11 09:56:43 +0300

    add

---
+++ 08.Iterators-and-Comparators/ComparableBook/Book.java
@@ -0,0 +1,55 @@
+package IteratorsAndComparators.ComparableBook;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book implements Comparable<Book>{
+    private String title;
+    private int year;
+    private List<String> authors = new ArrayList<>();
+
+    public Book(String title, int year, String... authors) {
+        this.title = title;
+        this.year = year;
+        setAuthors(authors);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setAuthors(String... authors) {
+        this.authors.addAll(Arrays.asList(authors));
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", year=" + year +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return Integer.compare(o.getYear(), this.getYear());
+    }
+}
